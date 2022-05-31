b94054fb5e709c16a5be6471cadea5f321910a5e
0xchristiaan <0xchristiaan@gmail.com>
2022-05-31 16:28:50 +0200

    initial commit

---
+++ Sophomore/NFT-collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x795b3123bB6aB1b8b0571206f50A91d8c5aD7167";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
